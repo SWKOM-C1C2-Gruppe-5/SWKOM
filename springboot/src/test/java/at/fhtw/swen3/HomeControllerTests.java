3916f1162799476b7b43437f977396ac0740c582
taqi <tthassan94@gmail.com>
2022-10-21 18:05:53 +0200

    fix testing

--- springboot/src/test/java/at/fhtw/swen3/HomeControllerTests.java
+++ springboot/src/test/java/at/fhtw/swen3/HomeControllerTests.java
@@ -1,7 +1,22 @@
 package at.fhtw.swen3;
 
+import at.fhtw.swen3.services.impl.HomeController;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class HomeControllerTests {
+
+    private HomeController homeController = new HomeController();
+    @Test
+    void testIndex() {
+        Assertions.assertEquals("redirect:swagger-ui.html", homeController.index());
+    }
+
+    @Test
+    void testIndexNotNull() {
+        Assertions.assertNotNull(homeController.index());
+    }
 }
