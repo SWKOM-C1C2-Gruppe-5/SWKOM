aa16abe4bbf128af0c535784924d00eb17601e8f
taqi <tthassan94@gmail.com>
2022-10-22 12:01:10 +0200

    create dtoToEntityTest for ParcelMapper

--- springboot/src/main/java/at/fhtw/swen3/services/mapper/BaseMapper.java
+++ springboot/src/main/java/at/fhtw/swen3/services/mapper/BaseMapper.java
@@ -2,7 +2,7 @@ package at.fhtw.swen3.services.mapper;
 
 import org.mapstruct.Mapper;
 
-@Mapper
+
 public interface BaseMapper<EN, DTO, DAL> {
     DTO entityToDto(EN entity);
     EN dtoToEntity(DTO dto);
--- springboot/src/test/java/at/fhtw/swen3/ParcelMapperTest.java
+++ springboot/src/test/java/at/fhtw/swen3/ParcelMapperTest.java
@@ -3,6 +3,7 @@ package at.fhtw.swen3;
 import at.fhtw.swen3.persistence.entity.ParcelEntity;
 import at.fhtw.swen3.persistence.entity.RecipientEntity;
 import at.fhtw.swen3.services.dto.Parcel;
+import at.fhtw.swen3.services.dto.Recipient;
 import at.fhtw.swen3.services.mapper.ParcelMapper;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
@@ -13,7 +14,6 @@ public class ParcelMapperTest {
 
     @Test
     void testEntityToDto() {
-        ParcelEntity parcelEntity = new ParcelEntity();
         RecipientEntity recipient = new RecipientEntity();
         recipient.setId(1);
         recipient.setName("Walter");
@@ -30,6 +30,7 @@ public class ParcelMapperTest {
         sender.setCity("Vienna");
         sender.setCountry("Austria");
 
+        ParcelEntity parcelEntity = new ParcelEntity();
         parcelEntity.setId(1);
         parcelEntity.setWeight(50F);
         parcelEntity.setRecipient(recipient);
@@ -37,5 +38,32 @@ public class ParcelMapperTest {
         Parcel parcel = ParcelMapper.INSTANCE.entityToDto(parcelEntity);
 
         Assertions.assertEquals(parcel.getWeight(), parcelEntity.getWeight());
+    }
+
+    @Test
+    void testDtoToEntity() {
+        Recipient recipient = new Recipient();
+        recipient.setName("Walter");
+        recipient.setStreet("Satoschigasse 83");
+        recipient.setPostalCode("1200");
+        recipient.setCity("Vienna");
+        recipient.setCountry("Austria");
+
+        Recipient sender = new Recipient();
+        sender.setName("Walruss");
+        sender.setStreet("Satoschigasse 84");
+        sender.setPostalCode("1210");
+        sender.setCity("Vienna");
+        sender.setCountry("Austria");
+
+        Parcel parcel = new Parcel();
+        parcel.setWeight(50F);
+        parcel.setRecipient(recipient);
+        parcel.setSender(sender);
+
+        ParcelEntity parcelEntity = ParcelMapper.INSTANCE.dtoToEntity(parcel);
+
+        Assertions.assertEquals(parcel.getWeight(), parcelEntity.getWeight());
+
     }
 }
