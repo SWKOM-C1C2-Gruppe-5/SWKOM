ecbee8b981b38697b782d7ba5a08c239cf6c6bd1
taqi <tthassan94@gmail.com>
2022-12-31 17:08:02 +0100

    temporary develop push

--- springboot/src/test/java/at/fhtw/swen3/services/mapper/GeoCoordinateMapperTest.java
+++ springboot/src/test/java/at/fhtw/swen3/services/mapper/GeoCoordinateMapperTest.java
@@ -1,21 +1,82 @@
 package at.fhtw.swen3.services.mapper;
 
+import at.fhtw.swen3.gps.service.Address;
+import at.fhtw.swen3.gps.service.impl.OpenStreetEncodingProxy;
 import at.fhtw.swen3.persistence.entities.GeoCoordinateEntity;
+import at.fhtw.swen3.persistence.entities.TruckEntity;
+import at.fhtw.swen3.persistence.entities.WarehouseEntity;
+import at.fhtw.swen3.persistence.repositories.HopRepository;
 import at.fhtw.swen3.persistence.repositories.TruckRepository;
 import at.fhtw.swen3.services.dto.GeoCoordinate;
+import at.fhtw.swen3.services.dto.Truck;
+import at.fhtw.swen3.services.dto.Warehouse;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Commit;
+
+import javax.transaction.Transactional;
+import java.io.IOException;
+import java.net.URISyntaxException;
 
 @SpringBootTest
+@Transactional
 class GeoCoordinateMapperTest
 {
-    /*
-    @Autowired
-    private TruckRepository truckRepository;
 
+    //@Autowired
+    //private TruckRepository truckRepository;
+
+    @Autowired
+    private HopRepository hopRepository;
+
+    @Test
+    void findAllHops() {
+        hopRepository.findAll();
+    }
+
+    @Test
+    @Commit
+    void addNewTruck() {
+        Truck truck = new Truck();
+        truck.setNumberPlate("MD180FW");
+        truck.setRegionGeoJson("Niederösterreich");
+        truck.setCode("CodeTruck");
+        truck.setDescription("descriptionTruck");
+        truck.setLocationName("Mödling");
+
+        TruckEntity truckEntity = TruckMapper.INSTANCE.dtoToEntity(truck);
+
+        hopRepository.save(truckEntity);
+
+        WarehouseEntity warehouseEntity = new WarehouseEntity();
+        warehouseEntity.setId(1);
+        warehouseEntity.setLevel(5);
+        warehouseEntity.setCode("123456Test1");
+        warehouseEntity.setDescription("BeschreibungWarehouse");
+
+        hopRepository.save(warehouseEntity);
+        hopRepository.findAll().forEach(System.out::println);
+    }
+
+    @Test
+    void openStreetEncodingProxyTest() throws IOException, URISyntaxException, InterruptedException {
+        Address address = new Address();
+        address.setStreet("87/Aspernstraße");
+        address.setPostalCode("1220");
+        address.setCity("Vienna");
+        address.setCountry("Austria");
+
+        OpenStreetEncodingProxy openStreetEncodingProxy = new OpenStreetEncodingProxy();
+
+        openStreetEncodingProxy.encodeAddress(address);
+
+    }
+
+
+    /*
     @Test
     void findAllTrucks() {
         truckRepository.findAll();
