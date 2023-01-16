e6aff383d136658c9e70eb3e884d87450b11e28a
taqi <tthassan94@gmail.com>
2023-01-16 21:10:22 +0100

    submit parcel

--- springboot/src/main/java/at/fhtw/swen3/controller/rest/ParcelApiController.java
+++ springboot/src/main/java/at/fhtw/swen3/controller/rest/ParcelApiController.java
@@ -1,10 +1,16 @@
 package at.fhtw.swen3.controller.rest;
 
 
+import at.fhtw.swen3.services.dto.Hop;
 import at.fhtw.swen3.services.dto.NewParcelInfo;
 import at.fhtw.swen3.services.dto.Parcel;
+import at.fhtw.swen3.services.exceptions.BLNotFoundException;
+import at.fhtw.swen3.services.exceptions.BLValidationException;
+import at.fhtw.swen3.services.impl.ParcelService;
 import at.fhtw.swen3.services.impl.ParcelServiceImpl;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.context.request.NativeWebRequest;
@@ -14,16 +20,18 @@ import javax.annotation.Generated;
 
 @Generated(value = "org.openapitools.codegen.languages.SpringCodegen", date = "2022-09-22T12:14:07.669603Z[Etc/UTC]")
 @Controller
+@Slf4j
 public class ParcelApiController implements ParcelApi {
 
     private final NativeWebRequest request;
 
-    //private final ParcelServiceImpl parcelServiceImpl;
 
-    @Autowired
-    public ParcelApiController(NativeWebRequest request /*, ParcelServiceImpl parcelServiceImpl*/) {
+    private final ParcelService parcelService;
+
+
+    public ParcelApiController(NativeWebRequest request , ParcelService parcelService) {
         this.request = request;
-        //this.parcelServiceImpl = parcelServiceImpl;
+        this.parcelService = parcelService;
     }
 
     @Override
@@ -31,12 +39,27 @@ public class ParcelApiController implements ParcelApi {
         return Optional.ofNullable(request);
     }
 
-    /*
     @Override
     public ResponseEntity<NewParcelInfo> submitParcel(Parcel parcel) {
-        parcelServiceImpl.createNewParcel(parcel);
-        return ParcelApi.super.submitParcel(parcel);
+        try {
+            try {
+                NewParcelInfo newParcelInfo = parcelService.submitParcel(parcel);
+                log.info("submitParcel() function called");
+                return new ResponseEntity<NewParcelInfo>(newParcelInfo, HttpStatus.CREATED);
+            } catch (BLValidationException e) {
+                log.error("The BLValidation failed: {}", e.getMessage());
+                return new ResponseEntity<NewParcelInfo>(HttpStatus.BAD_REQUEST);
+            }
+        } catch (Exception e) {
+            log.error("The operation failed due to an error: {}", e.getMessage());
+            return new ResponseEntity<NewParcelInfo>(HttpStatus.BAD_REQUEST);
+        }
     }
-    */
+
+    @Override
+    public ResponseEntity<NewParcelInfo> transitionParcel(String trackingId, Parcel parcel) {
+        NewParcelInfo newParcelInfo = parcelService.transferParcel(trackingId, parcel);
+        return new ResponseEntity<NewParcelInfo>(newParcelInfo, HttpStatus.OK);
+    }
 
 }
--- springboot/src/main/java/at/fhtw/swen3/services/impl/ParcelServiceImpl.java
+++ springboot/src/main/java/at/fhtw/swen3/services/impl/ParcelServiceImpl.java
@@ -1,29 +1,159 @@
 
 package at.fhtw.swen3.services.impl;
 
+import at.fhtw.swen3.gps.service.Address;
+import at.fhtw.swen3.gps.service.GeoEncodingService;
+import at.fhtw.swen3.gps.service.impl.OpenStreetEncodingProxy;
+import at.fhtw.swen3.persistence.entities.GeoCoordinateEntity;
 import at.fhtw.swen3.persistence.entities.ParcelEntity;
 import at.fhtw.swen3.persistence.repositories.ParcelRepository;
+import at.fhtw.swen3.persistence.repositories.RecipientRepository;
+import at.fhtw.swen3.services.dto.NewParcelInfo;
 import at.fhtw.swen3.services.dto.Parcel;
+import at.fhtw.swen3.services.dto.TrackingInformation;
+import at.fhtw.swen3.services.exceptions.BLValidationException;
 import at.fhtw.swen3.services.mapper.ParcelMapper;
 import at.fhtw.swen3.services.validation.MyValidator;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
+
+import java.util.UUID;
 
 @RequiredArgsConstructor
-@Service
+//@Service
 @Slf4j
 public class ParcelServiceImpl implements ParcelService {
-/*
+
+
     private final ParcelRepository parcelRepository;
+    private final MyValidator validator;
+    private final RecipientRepository recipientRepository;
 
-    @Autowired
-    private ParcelMapper parcelMapper;
+    //@Autowired
+    //private final OpenStreetEncodingProxy encodingProxy;
 
-    @Autowired
-    private MyValidator parcelValidator;
+    private String createTrackingId() {
+        // Generate a random UUID
+        UUID uuid = UUID.randomUUID();
+        // Convert the UUID to a 9-character alphanumeric string
+        String trackingId = uuid.toString().replaceAll("-", "").substring(0, 9).toUpperCase();
+        // Make sure the trackingId matches the pattern "^[A-Z0-9]{9}$"
+        if (!trackingId.matches("^[A-Z0-9]{9}$")) {
+            throw new IllegalArgumentException("The generated trackingId does not match the pattern \"^[A-Z0-9]{9}$\"");
+        }
+        /*
+        RandomStringGenerator trackingId = new RandomStringGenerator.Builder()
+                .withinRange('0', 'Z')
+                .filteredBy(CharacterPredicates.DIGITS, CharacterPredicates.LETTERS)
+                .build();
+         */
+        return trackingId;
+    }
 
+    @Override
+    public NewParcelInfo submitParcel(Parcel parcel) throws BLValidationException {
+        log.info("in function submitParcel()");
+
+        //map dto to entity
+        ParcelEntity parcelEntity = ParcelMapper.INSTANCE.dtoToEntity(parcel);
+        log.info("parcelDto mapped to parcelEntity");
+
+        // Create a new unique Tracking ID
+        String trackingId = createTrackingId();
+        log.info("trackingID created: " + trackingId);
+        parcelEntity.setTrackingId(trackingId);
+
+        // Set parcel state to "Pickup"
+        parcelEntity.setState(TrackingInformation.StateEnum.PICKUP);
+        log.info("parcel state set to Pickup");
+
+        try {
+            // Validate the parcel data
+            validator.validate(parcelEntity);
+            log.info("parcel validated");
+        } catch (BLValidationException e) {
+            log.error("The BLValidation failed: {}", e.getMessage());
+            throw new BLValidationException(e, "Validation failed!");
+        }
+        /*
+        // Get GPS coordinates for package sender/recipient
+        GeoCoordinateEntity senderCoordinates = encodingProxy.encodeAddress(new Address(
+                parcelEntity.getSender().getStreet(),
+                parcelEntity.getSender().getPostalCode(),
+                parcelEntity.getSender().getCity(),
+                parcelEntity.getSender().getCountry()
+        ));
+        log.info("SenderCoordinates Lat: " + senderCoordinates.getLat());
+        log.info("SenderCoordinates Lon: " + senderCoordinates.getLon());
+
+        GeoCoordinateEntity recipientCoordinates = encodingProxy.encodeAddress(new Address(
+                parcelEntity.getRecipient().getStreet(),
+                parcelEntity.getRecipient().getPostalCode(),
+                parcelEntity.getRecipient().getCity(),
+                parcelEntity.getRecipient().getCountry()
+        ));
+        log.info("RecipientCoordinates Lat: " + recipientCoordinates.getLat());
+        log.info("RecipientCoordinates Lon: " + recipientCoordinates.getLon());
+        */
+
+        // Predict future route between sender and recipient
+        //List<GeoCoordinateEntity> route = predictRoute(senderCoordinates, recipientCoordinates);
+
+        recipientRepository.save(parcelEntity.getRecipient());
+        recipientRepository.save(parcelEntity.getSender());
+
+        // Write data to database
+        parcelRepository.save(parcelEntity);
+        log.info("parcel data saved in database");
+
+        // Return the tracking ID in the response
+        NewParcelInfo newParcelInfo = new NewParcelInfo();
+        newParcelInfo.setTrackingId(trackingId);
+        return newParcelInfo;
+    }
+
+
+    @Override
+    public NewParcelInfo transferParcel(String trackingId, Parcel parcel) {
+/*
+        //map dto to entity
+        ParcelEntity parcelEntity = parcelMapper.dtoToEntity(parcel);
+        log.info("parcelDto mapped to parcelEntity");
+
+        // Validate the data
+        parcelValidator.validate(parcelEntity);
+        log.info("parcel validated");
+
+        // Reuse the existing tracking ID
+        parcelEntity.setTrackingId(trackingId);
+
+        // Get GPS coordinates for package sender/recipient
+        GeoEncodingService geoEncodingService = new OpenStreetEncodingProxy();
+        parcel.setSenderCoordinates(geoEncodingService.encodeAddress(parcel.getSender()));
+        parcel.setRecipientCoordinates(geoEncodingService.encodeAddress(parcel.getRecipient()));
+
+        // Predict future hops
+        parcelEntity.setFutureHops(predictRoute(parcel));
+
+        // Set parcel state to "Pickup"
+        parcelEntity.setState(TrackingInformation.StateEnum.PICKUP);
+        log.info("parcel state set to Pickup");
+
+        // Write data to database
+        parcelRepository.save(parcelEntity);
+
+        // Return Tracking ID
+        return new NewParcelInfo().trackingId(trackingId);
+
+ */
+        return null;
+    }
+
+
+
+
+/*
     public void createNewParcel(Parcel parcel) {
 
         //1. map dto to entity
